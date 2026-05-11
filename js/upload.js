221d1ed57f6b3c74a700813339509a8bf3a2eef7
TAYTISCHA <87822758+TAYTISCHA@users.noreply.github.com>
2026-05-11 21:08:25 +0700

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,1 +1,63 @@
+async function uploadFile(){
 
+    const year =
+        document.getElementById("year").value;
+
+    const semester =
+        document.getElementById("semester").value;
+
+    const subject =
+        document.getElementById("subject").value;
+
+    const file =
+        document.getElementById("file").files[0];
+
+    if(!file){
+
+        alert("กรุณาเลือกไฟล์");
+
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = async function(){
+
+        const base64 =
+            reader.result.split(",")[1];
+
+        const response = await fetch(
+            WEBAPP_URL,
+            {
+                method:"POST",
+
+                body:JSON.stringify({
+
+                    action:"upload",
+
+                    family:user.family,
+
+                    year,
+
+                    semester,
+
+                    subject,
+
+                    filename:file.name,
+
+                    mimeType:file.type,
+
+                    file:base64
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        alert(data.message);
+
+        loadFiles();
+    }
+
+    reader.readAsDataURL(file);
+}
