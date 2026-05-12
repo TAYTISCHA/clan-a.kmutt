78aa95e4d920de0b045a67d2e3d3d4ec77901541
TAYTISCHA <87822758+TAYTISCHA@users.noreply.github.com>
2026-05-12 23:41:52 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,57 +1,15 @@
-const user =
-    JSON.parse(localStorage.getItem("user"));
+function toggleSidebar(){
 
-if(!user){
-
-    window.location.href = "index.html";
+    document
+      .getElementById("sidebar")
+      .classList
+      .toggle("active");
 }
 
 function logout(){
 
-    localStorage.removeItem("user");
+    localStorage.clear();
 
-    window.location.href = "index.html";
-}
-
-loadFiles();
-
-async function loadFiles(){
-
-    const response = await fetch(WEBAPP_URL,{
-
-        method:"POST",
-
-        body:JSON.stringify({
-
-            action:"getFiles",
-
-            family:user.family
-        })
-    });
-
-    const data = await response.json();
-
-    let html = "";
-
-    data.files.forEach(file=>{
-
-        html += `
-
-        <div class="file-card">
-
-            <h3>${file.name}</h3>
-
-            <a href="${file.url}"
-               target="_blank">
-
-               เปิดไฟล์
-
-            </a>
-
-        </div>
-        `;
-    });
-
-    document.getElementById("file-list")
-        .innerHTML = html;
+    window.location.href =
+      "index.html";
 }
